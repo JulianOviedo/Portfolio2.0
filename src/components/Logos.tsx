a176124250edcd7f854371d4a635cf44708cac14
JulianOviedo <julianovie234@gmail.com>
2023-08-16 16:36:55 -0300

    add logos and icons

---
+++ src/components/Logos.tsx
@@ -0,0 +1,22 @@
+import { ExpressLogo } from "../assets/Icons/Icons"
+import { NodeLogo } from "../assets/Icons/Icons"
+import { MaterialUILogo } from "../assets/Icons/Icons"
+import { MySQLLogo } from "../assets/Icons/Icons"
+import { MongoLogo } from "../assets/Icons/Icons"
+import { NextLogo } from "../assets/Icons/Icons"
+import { CSSIcon, HtmlIcon, JavascriptIcon, ReactLogo, TailwindIcon, TypescriptIcon } from "../assets/Icons/Icons"
+
+export const Logos = () => {
+    const logos = [<HtmlIcon />, <CSSIcon />, <TailwindIcon />, <JavascriptIcon />, <TypescriptIcon />, <ReactLogo />, <NextLogo />, <NodeLogo />, <ExpressLogo />, <MongoLogo />, <MySQLLogo />, <MaterialUILogo />,]  
+      return (
+        <>
+            {logos.map(logo => {
+                return (
+                    <div className="w-16 lg:w-[84px]">
+                        {logo}
+                    </div>
+                )
+            })}
+        </>
+    )
+}
