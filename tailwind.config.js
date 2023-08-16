a269bbd54525d7564f98cd95642a37a13099b48f
JulianOviedo <julianovie234@gmail.com>
2023-08-16 16:36:41 -0300

    theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ export default {
   theme: {
     colors: {
       'primary' : 'rgb(115, 100, 208)' ,
-      'black': '#000',
+      'black': 'rgb(40, 40, 51)',
       'white': '#fff',
       'bg-light': 'rgb(246, 238, 238)',
-      'disabled': '#cccccc',
+      'disabled': 'rgb(158, 158, 158)',
+      'disabled-light': 'rgb(232, 232, 232)',
+      'bg-header': 'rgb(240, 230, 230)',
     },
     extend: {},
   },
