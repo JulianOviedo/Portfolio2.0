73ca42af4782267b9c664b1f2ea59bf462c44697
JulianOviedo <julianovie234@gmail.com>
2023-08-18 21:34:42 -0300

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -77,134 +77,134 @@ function App() {
   return (
     <div ref={ref} >
 
-    <div className="relative bg-bg-light lg:px-32 dark:bg-bg-dark dark:text-white">
+      <div className="relative bg-bg-light lg:px-32 dark:bg-bg-dark dark:text-white">
 
-      <header className="flex flex-row p-6 py-3 justify-between">
-        <div className="flex flex-row gap-4 items-center">
-          <Logo />
-          <h1 className="font-bold text-lg">Julian Oviedo</h1>
-        </div>
-
-        <nav className="flex gap-4 lg:gap-8 items-center">
-          <ul className="flex-row hidden gap-8 items-center text-disabled md:flex">
-            {NAV_LIs.map(navItem => {
-              return (
-                <li className={`${navItem === 'Home' ? 'text-black scale-105 border-b-2 dark:text-white' : 'transition-all duration-150 ease-in-out cursor-pointer hover:scale-105 hover:border-b-2'}`} key={navItem}>
-                  <a className="hover:text-black" href={`#${navItem}`}>{navItem}</a>
-                </li>
-              )
-            })}
-            <a href="mailto:julianovie234@gmail.com" className="bg-primary px-4 p-2 text-white rounded hover:shadow-xl hover:underline">Hire Me</a>
-          </ul>
-          <div className="flex flex-row gap-3 items-center ml-4 md:hidden">
-            <button onClick={handleBurgerMenu}>
-              <BurgerMenuIcon isOpen={isBurgerMenuOpen} />
-            </button>
+        <header className="flex flex-row p-6 py-3 justify-between">
+          <div className="flex flex-row gap-4 items-center">
+            <Logo />
+            <h1 className="font-bold text-lg">Julian Oviedo</h1>
           </div>
-          {darkMode
-            ? <DarkThemeIcon handleTheme={handleTheme} />
-            : <LighThemeIcon handleTheme={handleTheme}/>
-          }
-        </nav>
-      </header>
 
-      <div className="hidden lg:block">
-        <div className="fixed bottom-0 h-44 w-[2px] bg-disabled -ml-8 flex">
-          <div className="fixed bottom-48 flex flex-col gap-4 -ml-4">
-            <SocialMedia />
-          </div>
-        </div>
-        <div className="fixed right-24 bottom-0 h-44 w-[2px] bg-disabled ">
-          <div className="fixed bottom-48 -ml-2 flex flex-col gap-4">
-            <a href="#Home" data-box='Home' id="rotate" className="open w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
-            <a href="#About" data-box='About' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
-            <a href="#Skills" data-box='Skills' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
-            <a href="#My Work" data-box='My Work' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
-            <a href="#Contact" data-box='Contact' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
-
-          </div>
-        </div>
-      </div>
-
-      {isBurgerMenuOpen &&
-        <div id="burger-menu" className="z-20 absolute bg-white w-full h-full p-6">
-          <nav className="flex flex-col">
-            <ul className="flex-col items-center">
+          <nav className="flex gap-4 lg:gap-8 items-center">
+            <ul className="flex-row hidden gap-8 items-center text-disabled md:flex">
               {NAV_LIs.map(navItem => {
                 return (
-                  <li className='mb-4 transition-all duration-150 ease-in-out hover:scale-105' key={navItem}>
-                    <a onClick={handleBurgerMenu} className="hover:text-disabled cursor-pointer text-black" href={`#${navItem}`}>{navItem}</a>
+                  <li className={`${navItem === 'Home' ? 'text-black scale-105 border-b-2 dark:text-white' : 'transition-all duration-150 ease-in-out cursor-pointer hover:scale-105 hover:border-b-2'}`} key={navItem}>
+                    <a className="hover:text-black" href={`#${navItem}`}>{navItem}</a>
                   </li>
                 )
               })}
+              <a href="mailto:julianovie234@gmail.com" className="bg-primary px-4 p-2 text-white rounded hover:shadow-xl hover:underline">Hire Me</a>
             </ul>
-            <a href="mailto:julianovie234@gmail.com" className="bg-primary w-32 text-center mt-4 px-4 p-2 text-white rounded hover:shadow-xl hover:underline">Hire Me</a>
+            <div className="flex flex-row gap-3 items-center ml-4 md:hidden">
+              <button onClick={handleBurgerMenu}>
+                <BurgerMenuIcon isOpen={isBurgerMenuOpen} />
+              </button>
+            </div>
+            {darkMode
+              ? <DarkThemeIcon handleTheme={handleTheme} />
+              : <LighThemeIcon handleTheme={handleTheme} />
+            }
           </nav>
+        </header>
+
+        <div className="hidden lg:block">
+          <div className="fixed bottom-0 h-44 w-[2px] bg-disabled -ml-8 flex">
+            <div className="fixed bottom-48 flex flex-col gap-4 -ml-4">
+              <SocialMedia />
+            </div>
+          </div>
+          <div className="fixed right-24 bottom-0 h-44 w-[2px] bg-disabled ">
+            <div className="fixed bottom-48 -ml-2 flex flex-col gap-4">
+              <a href="#Home" data-box='Home' id="rotate" className="open w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
+              <a href="#About" data-box='About' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
+              <a href="#Skills" data-box='Skills' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
+              <a href="#My Work" data-box='My Work' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
+              <a href="#Contact" data-box='Contact' id="rotate" className="w-[20px] h-[20px] bg-disabled rounded hover:bg-black cursor-pointer transition-all duration-150"></a>
+
+            </div>
+          </div>
         </div>
-      }
 
-      <main className="p-6">
-
-        <section id="Home" className="text-xl flex h-screen items-start justify-center flex-col lg:ml-24">
-          <span className="text-primary font-bold text-xl lg:text-2xl">Hello! 👋🏻 My Name is</span>
-          <h1 className="font-extrabold text-4xl mt-4 lg:text-6xl">Julian Oviedo</h1>
-          <h2 className="mt-2 relative text-2xl font-bold lg:text-4xl dark:text-disabled">{titlesList[currentIndex]}<span id='typewritter' className="bg-bg-light after:bg-bg-dark dark:bg-bg-dark dark:after:bg-white">&#160;</span></h2>
-          <p className="mt-2 text-sm lg:text-lg dark:text-disabled">I design and build websites that look good, and work well.</p>
-          <a href="#My Work" className="text-white bg-primary rounded w-34 h-12 mt-4 font-extralight px-4 text-sm hover:shadow-2xl hover:underline flex items-center justify-center cursor-pointer lg:mb-56 lg:text-md">
-            See my Work
-          </a>
-        </section>
-
-        <section id="About" className="mb-40 lg:h-screen" >
-          <SectionTitle>About</SectionTitle>
-          <div className="md:flex md:flex-row-reverse md:gap-10 md:justify-end mt-10">
-            <img className="w-full lg:w-[450px] h-[350px] lg:h-[550px] lg:ml-20  rounded-md " src="/profile.jpeg" alt="profile img" />
-            <p className="my-6 text-md text-justify font-extralight lg:w-[45%] lg:text-lg dark:text-disabled">
-              👋🏻 I’m 26 years old. I’m currently studying Computer Science since almost 3 years and also have an Accountant degree. As you can see, I really like maths and everything related with numbers. <br /><br />
-
-              🌍  I'm based in Córdoba, Argentina <br /><br />
-
-              🖥 I'm a software developer with solid professional experience building web applications. My main focus is on the front-end, but i love building and learning about both ends of the stacks. <br /><br />
-
-              📚 I consider myself a very capable person to achieve everything i propose to and always want to learn new things. I think i only need the oportunity to introduce myself and show what I'm worth</p>
+        {isBurgerMenuOpen &&
+          <div id="burger-menu" className="z-20 absolute bg-white w-full h-full p-6 dark:bg-bg-dark">
+            <nav className="flex flex-col">
+              <ul className="flex-col items-center">
+                {NAV_LIs.map(navItem => {
+                  return (
+                    <li className='mb-4 transition-all duration-150 ease-in-out hover:scale-105' key={navItem}>
+                      <a onClick={handleBurgerMenu}  className={`${navItem === 'Home' ? 'hover:text-disabled cursor-pointer text-black dark:text-white border-b-2' : 'hover:text-disabled cursor-pointer text-black dark:text-disabled'}`} href={`#${navItem}`}>{navItem}</a>
+                    </li>
+                  )
+                })}
+              </ul>
+              <a href="mailto:julianovie234@gmail.com" className="bg-primary w-32 text-center mt-4 px-4 p-2 text-white rounded hover:shadow-xl hover:underline">Hire Me</a>
+            </nav>
           </div>
-        </section>
+        }
 
-        <section id="Skills" className="mb-40 lg:h-screen">
-          <SectionTitle>Skills</SectionTitle>
-          <div className="mt-20 flex gap-10 lg:gap-28 items-center flex-wrap justify-center lg:w-[70%] lg:m-auto lg:mt-20">
-            <Logos />
-          </div>
+        <main className="p-6">
 
-        </section>
+          <section id="Home" className="text-xl flex h-screen items-start justify-center flex-col lg:ml-24">
+            <span className="text-primary font-bold text-xl lg:text-2xl">Hello! 👋🏻 My Name is</span>
+            <h1 className="font-extrabold text-4xl mt-4 lg:text-6xl">Julian Oviedo</h1>
+            <h2 className="mt-2 relative text-2xl font-bold lg:text-4xl dark:text-disabled">{titlesList[currentIndex]}<span id='typewritter' className="bg-bg-light after:bg-bg-dark dark:bg-bg-dark dark:after:bg-white">&#160;</span></h2>
+            <p className="mt-2 text-sm lg:text-lg dark:text-disabled">I design and build websites that look good, and work well.</p>
+            <a href="#My Work" className="text-white bg-primary rounded w-34 h-12 mt-4 font-extralight px-4 text-sm hover:shadow-2xl hover:underline flex items-center justify-center cursor-pointer lg:mb-56 lg:text-md">
+              See my Work
+            </a>
+          </section>
 
-        <section id="My Work" className="mb-40 ">
-          <SectionTitle>My Work</SectionTitle>
-          <Proyects />
-        </section>
+          <section id="About" className="mb-40 lg:h-screen" >
+            <SectionTitle>About</SectionTitle>
+            <div className="md:flex md:flex-row-reverse md:gap-10 md:justify-end mt-10">
+              <img className="w-full lg:w-[450px] h-[350px] lg:h-[550px] lg:ml-20  rounded-md " src="/profile.jpeg" alt="profile img" />
+              <p className="my-6 text-md text-justify font-extralight lg:w-[45%] lg:text-lg dark:text-disabled">
+                👋🏻 I’m 26 years old. I’m currently studying Computer Science since almost 3 years and also have an Accountant degree. As you can see, I really like maths and everything related with numbers. <br /><br />
 
-        <section id="Contact" className="lg:mt-24 ">
-          <SectionTitle>Contact</SectionTitle>
-          <p className="mt-10">
-            I'm currently available to get involved in new projects, so get in touch if you'd like to work together. <br /><br />
-            Email me at <a className="text-primary font-bold hover:underline" href="mailto:julianovie234@gmail.com">julianovie234@gmail.com</a> and let's talk !
-          </p>
-        </section>
+                🌍  I'm based in Córdoba, Argentina <br /><br />
 
-        <hr className="w-full h-1 bg-disabled border-none my-14" />
+                🖥 I'm a software developer with solid professional experience building web applications. My main focus is on the front-end, but i love building and learning about both ends of the stacks. <br /><br />
 
-        <footer className="flex flex-col gap-4">
-          <Logo />
-          <span>© 2023 - Developed by Julian Oviedo</span>
-          <div className="flex flex-row gap-3 items-center lg:hidden">
-            <SocialMedia />
-          </div>
+                📚 I consider myself a very capable person to achieve everything i propose to and always want to learn new things. I think i only need the oportunity to introduce myself and show what I'm worth</p>
+            </div>
+          </section>
 
-        </footer>
+          <section id="Skills" className="mb-40 lg:h-screen">
+            <SectionTitle>Skills</SectionTitle>
+            <div className="mt-20 flex gap-10 lg:gap-28 items-center flex-wrap justify-center lg:w-[70%] lg:m-auto lg:mt-20">
+              <Logos />
+            </div>
+
+          </section>
+
+          <section id="My Work" className="mb-40 ">
+            <SectionTitle>My Work</SectionTitle>
+            <Proyects />
+          </section>
+
+          <section id="Contact" className="lg:mt-24 ">
+            <SectionTitle>Contact</SectionTitle>
+            <p className="mt-10">
+              I'm currently available to get involved in new projects, so get in touch if you'd like to work together. <br /><br />
+              Email me at <a className="text-primary font-bold hover:underline" href="mailto:julianovie234@gmail.com">julianovie234@gmail.com</a> and let's talk !
+            </p>
+          </section>
+
+          <hr className="w-full h-1 bg-disabled border-none my-14" />
+
+          <footer className="flex flex-col gap-4">
+            <Logo />
+            <span>© 2023 - Developed by Julian Oviedo</span>
+            <div className="flex flex-row gap-3 items-center lg:hidden">
+              <SocialMedia />
+            </div>
+
+          </footer>
 
 
-      </main>
-    </div>
+        </main>
+      </div>
     </div>
   )
 }
--- src/assets/Icons/Icons.tsx
+++ src/assets/Icons/Icons.tsx
@@ -7,13 +7,13 @@ export const Logo = () => {
 
 export const LighThemeIcon = ({ handleTheme }: any) => {
     return (
-        <svg onClick={handleTheme} className="w-9 h-9 fill-disabled hover:fill-white cursor-pointer" viewBox="0 0 24 24" stroke="gray"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+        <svg onClick={handleTheme} className="w-6 h-6 md:w-9 md-h-9 fill-disabled hover:fill-white cursor-pointer" viewBox="0 0 24 24" stroke="gray"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
     )
 }
 
 export const DarkThemeIcon = ({ handleTheme }: any) => {
     return (
-        <svg  onClick={handleTheme} className="w-9 h-9 cursor-pointer hover:fill-white fill-black" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+        <svg  onClick={handleTheme} className="w-6 h-6 md:w-9 md-h-9 cursor-pointer hover:fill-white fill-black" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
     )
 }
 
