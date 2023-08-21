bfb3a2f9d15d54c9b3f5fcf0dd9388c23be7c4ae
JulianOviedo <julianovie234@gmail.com>
2023-08-21 15:34:57 -0300

    ref timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -3,7 +3,7 @@ import { ExperienceItem } from "./ExperienceItem"
 export const Timeline = () => {
     return (
 
-        <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-40">
+        <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-40 md:ml-20">
             <ExperienceItem title="Frontend Software developer at Solvd inc." time="Since January 2022" latest={true} />
             <ExperienceItem title="React Javascript Intern at Solvd inc." time="June 2022 - December 2022" />
             <ExperienceItem title="Accountant at Centro de Estudios Filosoficos y Teologicos" time="August 2021 - January 2022" />
